bf1a820801c1fcfcb7375c233171dd6fe8f3b018
Rafael Matos Araújo <rafamatosaraujo@gmail.com>
2023-03-08 20:00:05 -0300

    setup navigation between splash and home fragment

--- app/src/main/java/com/example/my_movie_app/core/MainActivity.kt
+++ app/src/main/java/com/example/my_movie_app/core/MainActivity.kt
@@ -2,11 +2,20 @@ package com.example.my_movie_app.core
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import com.example.my_movie_app.R
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navHostFragment: NavHostFragment
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
     }
 }
